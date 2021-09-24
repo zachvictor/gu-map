d2669d5ed3184b8e279c7807cba1e017c840e34a
Zach Victor <zach@zvictor.com>
2021-09-24 12:30:17 -0700

    Use term "config/cfg" instead of "options/opt" for consistency with class names. Simplify means of reference to "internal" map object (no need for WeakMap). Block forEach calls from referring to internal "map" and potentially mutating state and instead force use of the proxy in the callback function.

--- gu-map.js
+++ gu-map.js
@@ -8,28 +8,26 @@ class GuMap extends Map {
    * The class name is a bilingual compound word, transliterating 固Map ("gù-map") into Latin letters.
    * 固 can mean strong, solid, and sure -- an allusion to this class's immutability-features.
    * @param {Array|Object} iterable
-   * @param {GuMapConfig|Object} options
+   * @param {GuMapConfig|Object} config
    * @return {Proxy} wrapped Map implementing dot notation for property accessors and enforcing property-level immutability on set.
    */
-  constructor(iterable, options) {
+  constructor(iterable, config) {
     super(iterable);
-    const id = {};
-    const _mapHolder = new WeakMap();
-    _mapHolder.set(id, this);
-    const _map = _mapHolder.get(id);
-    const opt = new GuMapConfig(options);
+    const _map = this; // "this" after super before proxy refers to the "internal" Map.
+    const cfg = new GuMapConfig(config);
     const errBase = 'GuMap Error:';
-    const errImmutable = () => opt.immutableMap ? ' Map is immutable.' : ' Properties are immutable.';
+    const errImmutable = () => cfg.immutableMap ? ' Map is immutable.' : ' Properties are immutable.';
     const errProp = (verb, key) => ` Cannot ${verb} property ${key}.`;
     const errNonexistent = (key) => ` Property ${key} does not exist.`;
     const bridge = {
       clear: () => _map.clear(),
       entries: () => _map.entries(),
-      forEach: (callbackFn, thisArg) => _map.forEach((value, key, map) => {
-        if (value !== map) {
-          callbackFn(value, key, map);
-        }
-      }, thisArg),
+      forEach: (callbackFn, thisArg) => {
+        // Ignore 3rd arg to block direct access to "internal" Map.
+        _map.forEach((value, key) => {
+          // Use mapProxy for 3rd arg to block direct access and prevent unwanted mutation.
+          callbackFn(value, key, mapProxy);
+        }, thisArg)},
       get: (getKey) => doGet(getKey),
       has: (hasKey) => _map.has(hasKey),
       keys: () => _map.keys(),
@@ -46,27 +44,27 @@ class GuMap extends Map {
     const doDelete = (key) => {
       let err, ret = false;
       // If Map/prop is immutable, don't delete prop, and prepare error msg (per options).
-      if (opt.immutableMap || opt.immutableProperties) {
-        err = opt.throwErrorOnPropertyMutate ? `${errBase}${errImmutable()}${errProp('delete', key)}` : '';
+      if (cfg.immutableMap || cfg.immutableProperties) {
+        err = cfg.throwErrorOnPropertyMutate ? `${errBase}${errImmutable()}${errProp('delete', key)}` : '';
         // If prop doesn't exist, then prepare error message (per options).
-      } else if (!_map.has(key) && opt.throwErrorOnNonexistentProperty) {
+      } else if (!_map.has(key) && cfg.throwErrorOnNonexistentProperty) {
         err = `${errBase}${errImmutable()}${errNonexistent('delete', key)}`;
-        // Default case: delete entry (nonexistent key returns false)
+        // Default case: delete entry (nonexistent key returns false).
       } else {
         ret = _map.delete(key);
       }
       return throwXorReturn(err, ret);
     }
-    const doGet = (key) => {
+    const doGet = (key/*, proxyHandler */) => {
       let err, ret = false;
       // Bridge native Map properties except Symbol.iterator (bridged properties cannot be overwritten).
       if (key in bridge) {
         ret = bridge[key];
       }
       // If prop doesn't exist, then prepare error msg (per options).
-      else if (!_map.has(key) && opt.throwErrorOnNonexistentProperty) {
+      else if (!_map.has(key) && cfg.throwErrorOnNonexistentProperty) {
         err = `${errBase}${errNonexistent(key)}${errProp('get', key)}`;
-        // Default case: native Map getter (nonexistent key returns undefined)
+        // Default case: native Map getter (nonexistent key returns undefined).
       } else {
         ret = _map.get(key);
       }
@@ -75,28 +73,29 @@ class GuMap extends Map {
     const doSet = (key, value) => {
       let err, ret = false;
       // If Map is immutable, or if props are immutable and prop exists, then don't set prop, and prepare error msg (per options).
-      if (opt.immutableMap || (opt.immutableProperties && _map.has(key)) || key in bridge) {
-        err = opt.throwErrorOnPropertyMutate ? `${errBase}${errImmutable()}${errProp('set', key)}` : '';
-        // Default case: super behavior for deleting an entry
+      if (cfg.immutableMap || (cfg.immutableProperties && _map.has(key)) || key in bridge) {
+        err = cfg.throwErrorOnPropertyMutate ? `${errBase}${errImmutable()}${errProp('set', key)}` : '';
+      // Default case: super behavior for deleting an entry.
       } else {
         ret = _map.set(key, value);
       }
       return throwXorReturn(err, ret);
     };
-    return new Proxy(this, {
+    const mapProxy = new Proxy(this, {
       deleteProperty(target, key) {
         return doDelete(key);
-      },
-      get(target, key) {
-        return doGet(key);
       },
       set(target, key, value) {
         return doSet(key, value);
       },
       setPrototypeOf() {
         throw new Error(`${errBase} The prototype cannot be changed.`);
+      },
+      get(target, key) {
+        return doGet(key);
       }
     });
+    return mapProxy;
   }
 }
 
